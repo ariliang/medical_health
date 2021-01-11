609eef70192482cc3363d1e3ef923f4f4c44ead8
ariliang <ariliangx@gmail.com>
2021-01-11 13:01:24 +0800

    questions parsed

--- beautify.py
+++ beautify.py
@@ -1,9 +1,56 @@
 #! /usr/bin/env python3
 
 import pandas as pd
+import re
+
+
+def parse_nothing(text):
+    return text
 
 def parse_que(que):
-    return que
+    results = {}
+    # 统一分隔符避免分割错误
+    que = que.replace(':', '：')
+    # 所有的键
+    keys = ['状态', '咨询标题', '疾病', '时长', '怀孕情况', '过敏史', '病情描述', '希望提供的帮助',
+            '所就诊医院科室', '治疗情况', '医院科室', '好大夫在线友情提示',
+            '如何上传', '治疗过程', '用药情况', '药物名称', '服用说明', '既往病史', '检查资料']
+    for k in keys:
+        temp = k+'：'
+        # 若此键在问题中，则将其作为分隔符分成两部分
+        # 取第二部分，再用'：'分开，取第一部分
+        # 再将目标后部分的键去掉
+        # 如:
+        #   状态：就诊前2017-01-15咨询标题：咨询标题：需要手术吗疾病
+        # 分割后为:
+        #   就诊前2017-04-11咨询标题
+        if temp in que:
+            # 分割两次，先后取第二、第一部分
+            nearly_target = que.split(temp)[1].split('：')[0]
+
+            target = nearly_target
+            for m in keys:
+                # 去掉后部分的键
+                ptn = re.compile(r'(.*)(%s$)' % m)
+                res = re.search(ptn, target)
+                # 若键存在，取group 1
+                if res:
+                    target = res.group(1)
+                    break
+            # 过滤无用键值
+            if k not in ['好大夫在线友情提示', '如何上传']:
+                results[k] = target
+
+    # 分离时间
+    ptn = re.compile(r'(.*?)(\d{4}-\d{2}-\d{2})(.*)')
+    text = results.get('状态')
+    results['状态'] = re.search(ptn, text).group(1)
+    results['时间'] = re.search(ptn, text).group(2)
+    # 若是追问，分离追问内容
+    temp = re.search(ptn, text).group(3)
+    if temp:
+        results['内容'] = temp
+    return results
 
 def parse_ans(ans):
     return ans
@@ -11,19 +58,20 @@ def parse_ans(ans):
 def parse_qa(qa_list):
     results = []
     # 遍历一个对话，将解析的对话列表作为一个字典，存入results
-    for item in qa_list:
+    for i, item in enumerate(qa_list):
         piece = {}
         # 若某条对话是空字符或None则跳过
         if not item:
             continue
         # 表示提问者，这一条为问题
         if '***' in item:
-            piece['desc'] = parse_que(item)
             piece['is_que'] = 1
+            # piece['desc'] = parse_nothing(item)
+            piece['desc'] = parse_que(item)
         # 这一条为回答
         else:
-            piece['desc'] = parse_ans(item)
             piece['is_que'] = 0
+            piece['desc'] = parse_ans(item)
         # 将此条加入结果集
         results.append(piece)
     return results
@@ -47,13 +95,10 @@ def main():
         qa = item.split(delimiter)
         results = parse_qa(qa)
 
-        # for res in results:
-        #     print(res)
         for res in results:
-            if res.get('is_que'):
-                print('q:', res.get('desc'))
-            else:
-                print('a:', res.get('desc'))
+            # if res.get('is_que'):
+            #     print(res.get('desc'))
+            print(res)
         print()
 
 if __name__ == '__main__':
